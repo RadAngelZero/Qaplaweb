1c9282d099ae9d255e242521d88ec25b93636377
DHVSGG <diego@qapla.gg>
2022-09-03 14:28:41 -0500

    App return returned

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
 import { useState, useEffect, useMemo } from 'react';
-
+import logo from './logo.svg';
 import './App.css';
 import { getUserProfile } from './services/database';
 import { listenToAuthState } from './services/auth';
@@ -10,7 +9,6 @@ function useQuery() {
 
     return useMemo(() => new URLSearchParams(search), [search]);
 }
-
 
 function App() {
     const [user, setUser] = useState(undefined);
@@ -39,7 +37,16 @@ function App() {
         }
     }, [user, query]);
 
-    
+    return (
+        <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            <code>Qapla Web</code>
+            </p>
+        </header>
+        </div>
+    );
 }
 
 export default App;
