47a2749e22c2a36c55b742819cae76f8dc2274f1
jose <joseandrescolmenares02@gmail.com>
2022-09-03 17:06:07 -0300

    comenzando DeQButtonPayment

---
+++ src/components/DeQButtonPayments/DeQButtonPayments.jsx
@@ -0,0 +1,13 @@
+  import React from "react";
+
+  const DeQButtonPayments = () => {
+    return (
+        <div style={{borderRadius:'20px', maxWidth: '92px', height: '37px'}}>
+            <div>
+                <h2>200</h2>
+            </div>
+        </div>
+    )
+  }
+
+  export default DeQButtonPayments;
